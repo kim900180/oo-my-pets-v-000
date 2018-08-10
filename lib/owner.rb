81d40230338529620dcc2eae80b4310e437eca10
Jae Yong Kim <kim900180@gmail.com>
2018-08-10 14:38:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
 class Owner
-  # code goes here
+  # code goes here(
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  OWNERS = []
+
+
+  def initialize(species)
+    @species = species
+    OWNERS << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def self.all
+    OWNERS
+  end
+
+  def self.count
+    OWNERS.size
+  end
+
+  def self.reset
+    OWNERS.clear
+  end
+
+  def say_species
+    @species
+  end
+
+  def name
+    @name
+  end
+
+  def pets
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def walk_dogs
+    pets[:dongs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+
+  end
+
+  def list_pets
+    @pets
+  end
 end
